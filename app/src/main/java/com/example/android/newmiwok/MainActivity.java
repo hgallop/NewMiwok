0b2fc2d566f85b6a8e2a5cc32aa118a3dcbf2882
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-13 14:09:43 -0500

    Remove and replace default onClick behavior with custom on click behavior inside of onCreate method in MainActivity.

--- app/src/main/java/com/example/android/newmiwok/MainActivity.java
+++ app/src/main/java/com/example/android/newmiwok/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,25 +13,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = findViewById(R.id.numbers);
+        TextView colors = findViewById(R.id.colors);
+        TextView phrases = findViewById(R.id.phrases);
+        TextView familyMembers = findViewById(R.id.family);
+
+        numbers.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        colors.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+        phrases.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent PhrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(PhrasesIntent);
+            }
+        });
+
+        familyMembers.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent familyMembersIntent = new Intent(MainActivity.this, FamilyMembersActivity.class);
+                startActivity(familyMembersIntent);
+            }
+        });
     }
 
-    public void openNumbersList(View view) {
-        Intent numbers = new Intent(this, NumbersActivity.class);
-        startActivity(numbers);
-    }
-
-    public void openFamilyMembersList(View view){
-        Intent numbers = new Intent(this, FamilyMembersActivity.class);
-        startActivity(numbers);
-    }
-
-    public void openColorsList(View view){
-        Intent numbers = new Intent(this, ColorsActivity.class);
-        startActivity(numbers);
-    }
-
-    public void openPhrasesList(View view){
-        Intent numbers = new Intent(this, PhrasesActivity.class);
-        startActivity(numbers);
-    }
 }
