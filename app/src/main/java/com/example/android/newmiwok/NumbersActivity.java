33add329dcdd66a5b542a07fde087406895a113a
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-13 15:23:17 -0500

    Add Log message for each element in the words array.

--- app/src/main/java/com/example/android/newmiwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/newmiwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.newmiwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +25,17 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+
+        //creates log messages for each element in the array
+        Log.v("NumbersActivity", "Word at index 0 is: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1 is: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2 is: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3 is: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4 is: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 5 is: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 6 is: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 7 is: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 8 is: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 9 is: " + words[9]);
     }
 }
