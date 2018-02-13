2c65a6e6390deb8cb5f94012c874a8840135db89
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-13 11:57:09 -0500

    Add onClick methods for each of the TextViews in activity_main with intent to open each individual activity for Numbers, Colors, Family Members, and phrases.

--- app/src/main/java/com/example/android/newmiwok/MainActivity.java
+++ app/src/main/java/com/example/android/newmiwok/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.newmiwok;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+        Intent numbers = new Intent(this, NumbersActivity.class);
+        startActivity(numbers);
+    }
+
+    public void openFamilyMembersList(View view){
+        Intent numbers = new Intent(this, FamilyMembersActivity.class);
+        startActivity(numbers);
+    }
+
+    public void openColorsList(View view){
+        Intent numbers = new Intent(this, ColorsActivity.class);
+        startActivity(numbers);
+    }
+
+    public void openPhrasesList(View view){
+        Intent numbers = new Intent(this, PhrasesActivity.class);
+        startActivity(numbers);
     }
 }
