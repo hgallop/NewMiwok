0dc37655870b57530a08c53bd0e49a6bc8dfe724
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-14 08:08:02 -0500

    Update comments in NumbersActivity.

--- app/src/main/java/com/example/android/newmiwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/newmiwok/NumbersActivity.java
@@ -2,11 +2,8 @@ package com.example.android.newmiwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,7 +15,7 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //declare a new array list of strings
-        ArrayList<String>words = new ArrayList<String>();
+        ArrayList<String>words = new ArrayList<>();
 
         //initialize each index of the array list
         words.add("one");
@@ -32,10 +29,10 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        //creates a view for each array list item
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        //create an array adapter object that makes a layout of a single text view, predefined by android, for each array list item in words
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
 
-        //calls the list view layout from activity_numbers
+        // createa a variable for the list view layout from activity_numbers
         ListView listView = findViewById(R.id.list);
 
         //sets the array adapter to the list view layout
