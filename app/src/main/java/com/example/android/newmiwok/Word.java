1a172b93894e7d69e50fc8135070041b30110f7e
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-16 12:46:00 -0500

    Alter list_item to add ImageView. Add drawable resources for image icons. Add second constructor to class word to allow for image. Alter Arraylists for Numbers, Colors, and Family Members to include image from drawables resource.

--- app/src/main/java/com/example/android/newmiwok/Word.java
+++ app/src/main/java/com/example/android/newmiwok/Word.java
@@ -6,14 +6,21 @@ package com.example.android.newmiwok;
 //creates class for Word. used to display default and mowok translation
 public class Word {
 
-    //create variables for each translation
+    //create variables for each translation and the image to describe them
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
     //constructor for Word object
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
 
     //accessor. allows default translation to be accessed.
@@ -24,5 +31,10 @@ public class Word {
     //accessor. allows miwok translation to be accessed
     public String getMiwokTranslation(){
         return mMiwokTranslation;
+    }
+
+    //accessor.allows image resource id to be accessed
+    public int getImageResourceId(){
+        return mImageResourceId;
     }
 }
