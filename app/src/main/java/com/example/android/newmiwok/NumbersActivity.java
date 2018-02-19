ef9666158f3f52e7eec6bd335dd6e3ba549140b9
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-19 08:19:13 -0500

    Create a completion listener method for each category. Implement completion listener on media player. Set each category media player to release after each clip is played.

--- app/src/main/java/com/example/android/newmiwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/newmiwok/NumbersActivity.java
@@ -13,6 +13,15 @@ public class NumbersActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
 
+    //creates callback for determining when player has finished
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            //calls method to check if not null, then release and set to null
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,10 +54,30 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                //calls method to check if not null, then release and set to null
+                releaseMediaPlayer();
                 Word word = words.get(pos);
                 mediaPlayer =  mediaPlayer.create(NumbersActivity.this, word.getMiwokAudio());
                 mediaPlayer.start();
+                //checks to see if file has finished playing
+                mediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
--- app/src/main/java/com/example/android/newmiwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/newmiwok/PhrasesActivity.java
@@ -6,12 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+
+    //creates callback for determining when player has finished
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            //calls method to check if not null, then release and set to null
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +55,32 @@ public class PhrasesActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                //calls method to check if not null, then release and set to null
+                releaseMediaPlayer();
                 Word word = words.get(pos);
                 mediaPlayer =  mediaPlayer.create(PhrasesActivity.this, word.getMiwokAudio());
                 mediaPlayer.start();
+                //checks to see if file has finished playing
+                mediaPlayer.setOnCompletionListener(mCompletionListener);
             }
+
         });
+
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
